7d316af59cbaf5973c36e293b56f9456c1e6a0fb
Dchole <derekoware47@gmail.com>
2021-02-25 08:26:29 +0000

    MaterialUI setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import "@fontsource/montserrat/600.css";
+import "@fontsource/roboto";
+
+import Head from "next/head";
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "@/lib/theme";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
